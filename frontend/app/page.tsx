548f4efa27eb7615c32c378c443a745877f56c88
Sucram314 <99621225+Sucram314@users.noreply.github.com>
2025-09-20 22:21:06 -0400

    make markers better

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -55,7 +55,7 @@ const HomePage: React.FC = () => {
   const clusterSvg = (count: number) => `<svg viewBox="-4 0 36 36">
     <path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
     <circle fill="black" cx="14" cy="14" r="7"></circle>
-    <text x="14" y="18" text-anchor="middle" fill="white" font-size="10" font-weight="bold">${count}</text>
+    <text x="14" y="18" text-anchor="middle" fill="white" font-size="12" font-weight="bold">${count}</text>
   </svg>`;
 
   const clusterBase = 1500;
@@ -258,8 +258,8 @@ const HomePage: React.FC = () => {
             el.innerHTML = markerSvg;
           }
           
-          el.style.color = d.registered ? "#41fc03" : "#fc0303";
-          el.style.width = d.count > 1 ? `40px` : `30px`;
+          el.style.color = d.registered ? "#2eb700ff" : "#fc0303";
+          el.style.width = `${40+d.count/200}px`;
           el.style.height = 'auto';
           el.style.cursor = 'pointer';
           el.style.pointerEvents = 'auto';
